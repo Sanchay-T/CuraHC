04333e4a7bcd5f5a1ddd3bf9f562de69e043c95b
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2024-04-29 01:31:19 +0530

    Refactor ProfileCompletionMiddleware in middleware.py

--- middleware.py
+++ middleware.py
@@ -1,13 +1,28 @@
-from django.shortcuts import redirect
+from doctorportal.models import Doctor
 from django.urls import reverse
+
 
 class ProfileCompletionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated and not hasattr(request.user, 'doctor_profile'):
-            if request.path not in [reverse('doctor_add'), reverse('logout'), reverse('login')]:
-                return redirect('doctor_add')
         response = self.get_response(request)
+
+        if not request.user.is_authenticated:
+            return response
+
+        if request.user.is_superuser:
+            return response
+
+        if Doctor.objects.filter(user=request.user).exists():
+            return response
+
+        if request.path not in [
+            reverse("doctor_add"),
+            reverse("logout"),
+            reverse("login"),
+        ]:
+            return redirect("doctor_add")
+
         return response
